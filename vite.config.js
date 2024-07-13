2f6f70f47233b49a200d5242058e7584ea0e6d67
Danico san <mfaladan@hotmail.com>
2024-07-13 14:43:53 +0300

    config vite test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.ts",
+    exclude: [...configDefaults.exclude, "e2e/**"],
+  },
 });
